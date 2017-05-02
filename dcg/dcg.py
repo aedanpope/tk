65c141b38dd8f39fcede4493b824d2b04a368a74
Aedan Pope <aedanpope@gmail.com>
2017-05-02 22:59:52 +0100

    working grad graph for y=mx+c

--- dcg/dcg.py
+++ dcg/dcg.py
@@ -2,45 +2,62 @@
 
 # E.g. Matrix multiplication.
 
-# Functons need to define eval(), and some jacobian shit..
+# Functons need to define inner_eval(), and inner_derivative()
+
+class Function:
+  def eval(self, input_vals):
+    val = self.inner_eval(input_vals)
+    return val
+
+  def derivative(self, input_vals, wrt_input_index):
+    if not input_vals: raise Exception("Can't compute derivative with no inputs")
+    val = self.inner_derivative(input_vals, wrt_input_index)
+    return val
+
+  def __str__(self):
+    return self.__class__.__name__
+
 
 # Constant functions.
-
-class Placeholder:
+class Placeholder(Function):
   placeholder_value = None
   def __init__(self, value):
     self.placeholder_value = value
 
-  def eval(self, input_vals):
-    print "eval placeholder " + str(self.placeholder_value)
+  def inner_eval(self, input_vals):
     return self.placeholder_value
 
-class Variable:
+
+class Variable(Function):
   variable_value = None
   def __init__(self, value):
     self.variable_value = value
 
-  def eval(self, input_vals):
-    print "eval variable " + str(self.variable_value)
+  def inner_eval(self, input_vals):
     return self.variable_value
 
 
 # Aggregate functions
-class Plus:
-  def eval(self, input_vals):
-    print "eval sum " + str(input_vals) + " = " + str(sum(input_vals))
+class Plus(Function):
+  def inner_eval(self, input_vals):
     return sum(input_vals)
 
+  def inner_derivative(self, input_vals, wrt_input_index):
+    return 1
 
-class Multiply:
-  def eval(self, input_vals):
+
+class Multiply(Function):
+  def inner_eval(self, input_vals):
     if len(input_vals) < 1:
-      raise Exception("can't multiply 0 input vals")
+      raise Exception("can't multiply no input vals")
     product = input_vals[0];
     for v in input_vals[1:]:
       product *= v
-    print "eval multiply " + str(input_vals) + " = " + str(product)
     return product
+
+  def inner_derivative(self, input_vals, wrt_input_index):
+    input_vals[wrt_input_index] = 1
+    return self.inner_eval(input_vals)
 
 
 class Operation:
@@ -56,6 +73,23 @@ class Operation:
     self.inputs = []
     self.consumers = []
 
+  def __str__(self):
+    return ("Operation {" +
+        str(self.identifier) + ", " +
+        "function: " + str(self.function) + ", " +
+        "value: " + str(self.value) + ", " +
+        "gradient: " + str(self.gradient) + "}")
+
+  def derivative(self, input_op):
+    """ Given that the gradient of this op is know, returns
+        The gradient of the input_op with respect to this op.
+        input_op must be in self.inputs
+    """
+    if not input_op in self.inputs: raise Exception("input_op not in self.inputs")
+
+    return self.gradient * self.function.derivative(
+        [inp.value for inp in self.inputs], self.inputs.index(input_op))
+
 
 class DifferentiableComputationGraph:
   all_ops = None
@@ -65,7 +99,9 @@ class DifferentiableComputationGraph:
     self.all_ops = []
 
   def add_op(self, inputs, function, identifier=None):
-    if identifier is None: identifier = self.next_identifier ++
+    if identifier is None:
+      identifier = "e" + str(self.next_identifier)
+      self.next_identifier += 1
     op = Operation(identifier)
     op.inputs = inputs
     op.function = function
@@ -74,47 +110,54 @@ class DifferentiableComputationGraph:
     self.all_ops.append(op)
     return op
 
-  def recursive_evaluate(self, op):
+  def _evaluate_recursive(self, op):
     if op.value is not None: return op.value
 
     input_vals = []
     for inp_op in op.inputs:
-      input_vals.append(self.recursive_evaluate(inp_op))
+      input_vals.append(self._evaluate_recursive(inp_op))
     op.value = op.function.eval(input_vals)
+    return op.value
 
   def evaluate(self, output_op):
     # should assert that output_op is of type Placeholder
     # Reset the graph.
     for op in self.all_ops:
       op.value = None
-    self.recursive_evaluate(output_op)
+    self._evaluate_recursive(output_op)
     return output_op.value
 
-  def compute_gradient(self, op):
+  def _compute_gradient_recursive(self, op):
     if op.gradient is not None:
       return op.gradient
 
     op.gradient = 0
 
     for consumer in op.consumers:
-      self.compute_gradient(consumer)
-      # op.gradient += consumer.
+      self._compute_gradient_recursive(consumer)
+      op.gradient += consumer.derivative(op)
 
+    return op.gradient
 
-
-  def backprop(self, output_op, output_grad):
+  def compute_gradients(self, output_op, output_grad):
     """ Populates gradients field in all the ops in the graph.
     """
-    # Reset
     for op in self.all_ops:
       op.gradient = None
 
     # Assign gradient on output.
-    output_op.grad = output_grad
+    output_op.gradient = output_grad
 
     # Compute all gradients.
     for op in self.all_ops:
-      self.compute_gradient(op)
+      # No loops because graph is a DAG
+      self._compute_gradient_recursive(op)
+
+  def backprop(self, output_op, output_grad, learning_rate):
+    self.compute_gradients(output_op, output_grad)
+    for op in self.all_ops:
+      if isinstance(op.function, Variable):
+        op.function.variable_value += (op.gradient * learning_rate)
 
 
 
--- dcg/sample_graph.py
+++ dcg/sample_graph.py
@@ -1,14 +1,22 @@
 import dcg
+import random
 
-lin_graph = dcg.DifferentiableComputationGraph()
+gp = dcg.DifferentiableComputationGraph()
 
-x_inp = lin_graph.add_op([], dcg.Placeholder(5), 'x_in')
+x_inp = gp.add_op([], dcg.Placeholder(5), 'x_in')
+m_var = gp.add_op([], dcg.Variable(3), 'm')
+c_var = gp.add_op([], dcg.Variable(12), 'c')
+y_out = gp.add_op([gp.add_op([x_inp,m_var], dcg.Multiply()),c_var], dcg.Plus(), 'y_out')
 
-m_var = lin_graph.add_op([], dcg.Variable(3), 'm')
-c_var = lin_graph.add_op([], dcg.Variable(12), 'c')
-y_out = lin_graph.add_op([lin_graph.add_op([x_inp,m_var], dcg.Multiply()),c_var], dcg.Plus(), 'y_out')
+real_m = 8
+real_c = -4
 
-print "y_out = " + str(lin_graph.evaluate(y_out))
-x_inp.function.placeholder_value = -5
-print "x = -5"
-print "y_out = " + str(lin_graph.evaluate(y_out))
+for i in range(0,1000):
+  x = (random.random()-0.5)*10
+  x_inp.function.placeholder_value = x
+  y = gp.evaluate(y_out)
+  real_y = real_m * x + real_c
+  loss = real_y - y
+  gp.backprop(y_out, loss, 0.01)
+  print ("x = %3d, y = %3d, m_var = %f, c_var = %f, loss = %f" %
+          (x, y, m_var.function.variable_value, c_var.function.variable_value, loss))
